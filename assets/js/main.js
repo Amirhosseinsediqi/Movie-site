9841390154987f375764bafbc32e3340cf91f7f4
Amir hossein <87437145+3amin@users.noreply.github.com>
2022-11-18 20:42:26 +0330

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function(){
+    if(window.pageYOffset > 100){
+        nav.classList.add('bg-dark', 'shadow');
+    }else{
+        nav.classList.remove('bg-dark', 'shadow');
+    }
+})
+
+
+
+
+
